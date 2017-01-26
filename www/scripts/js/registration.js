ea0a545778e906844e6cb210efdd9e68ce37fbc7
VATFrancisEspiritu <francis.espiritu@valtes.com.ph>
2017-01-26 14:44:28 +0800

    Update registration.js

--- www/scripts/js/registration.js
+++ www/scripts/js/registration.js
@@ -6,90 +6,92 @@
     "use strict";
 
     document.addEventListener('init', function (event) {
-        document.querySelector('#cancelButton').onclick = function () {
-            document.querySelector('#myNavigator').popPage().then(function () { });
-        };
+        var page = event.target;
+        if (page.id === 'registration') {
+            document.querySelector('#cancelButton').onclick = function () {
+                document.querySelector('#myNavigator').popPage().then(function () { });
+            };
 
-        document.querySelector('#registerButton').onclick = function () {
-            insertToDB();
-        };
+            document.querySelector('#registerButton').onclick = function () {
+                insertToDB();
+            };
 
-        document.querySelector('#closeButton').onclick = function () {
-            document.querySelector('#modal').hide();
-        };
+            document.querySelector('#closeButton').onclick = function () {
+                document.querySelector('#modal').hide();
+            };
 
-        document.querySelector('#ccnumber').onblur = function () {
-            var ccnumberInput = $('#ccnumber').val();
-            var ccnumberError = "";
-            if ("" == ccnumberInput || null == ccnumberInput) {
-                ccnumberError = "Please input your Credit Card or Agreement Number."
-            }
-            document.querySelector('#ccnumberErrorLabel').style.display = "inline";
-            document.querySelector('#ccnumberErrorLabel').innerText = ccnumberError;
-        };
+            document.querySelector('#ccnumber').onblur = function () {
+                var ccnumberInput = $('#ccnumber').val();
+                var ccnumberError = "";
+                if ("" == ccnumberInput || null == ccnumberInput) {
+                    ccnumberError = "Please input your Credit Card or Agreement Number."
+                }
+                document.querySelector('#ccnumberErrorLabel').style.display = "inline";
+                document.querySelector('#ccnumberErrorLabel').innerText = ccnumberError;
+            };
 
-        document.querySelector('#fullname').onblur = function () {
-            var fullnameInput = $('#fullname').val();
-            var fullnameError = "";
-            if ("" == fullnameInput || null == fullnameInput) {
-                fullnameError = "Please input your Full Name."
-            }
-            document.querySelector('#fullnameErrorLabel').style.display = "inline";
-            document.querySelector('#fullnameErrorLabel').innerText = fullnameError;
-        };
+            document.querySelector('#fullname').onblur = function () {
+                var fullnameInput = $('#fullname').val();
+                var fullnameError = "";
+                if ("" == fullnameInput || null == fullnameInput) {
+                    fullnameError = "Please input your Full Name."
+                }
+                document.querySelector('#fullnameErrorLabel').style.display = "inline";
+                document.querySelector('#fullnameErrorLabel').innerText = fullnameError;
+            };
 
-        document.querySelector('#passwordField').onblur = function () {
-            var passwordInput = $('#passwordField').val();
-            var passwordError = "";
-            if ("" == passwordInput || null == passwordInput) {
-                passwordError = "Please input your Password."
-            }
-            document.querySelector('#passwordErrorLabel').style.display = "inline";
-            document.querySelector('#passwordErrorLabel').innerText = passwordError;
-        };
+            document.querySelector('#passwordField').onblur = function () {
+                var passwordInput = $('#passwordField').val();
+                var passwordError = "";
+                if ("" == passwordInput || null == passwordInput) {
+                    passwordError = "Please input your Password."
+                }
+                document.querySelector('#passwordErrorLabel').style.display = "inline";
+                document.querySelector('#passwordErrorLabel').innerText = passwordError;
+            };
 
-        document.querySelector('#repassword').onblur = function () {
-            var repasswordInput = $('#repassword').val();
-            var passwordInput = $('#passwordField').val();
-            var repasswordError = "";
-            if (repasswordInput != passwordInput) {
-                repasswordError = "Inputted Passwords do not match."
-            }
-            document.querySelector('#repasswordErrorLabel').style.display = "inline";
-            document.querySelector('#repasswordErrorLabel').innerText = repasswordError;
-        };
+            document.querySelector('#repassword').onblur = function () {
+                var repasswordInput = $('#repassword').val();
+                var passwordInput = $('#passwordField').val();
+                var repasswordError = "";
+                if (repasswordInput != passwordInput) {
+                    repasswordError = "Inputted Passwords do not match."
+                }
+                document.querySelector('#repasswordErrorLabel').style.display = "inline";
+                document.querySelector('#repasswordErrorLabel').innerText = repasswordError;
+            };
 
-        document.querySelector('#email').onblur = function () {
-            var emailInput = $('#email').val();
-            var emailError = "";
-            if ("" == emailInput || null == emailInput) {
-                emailError = "Please input your Email."
-            }
-            document.querySelector('#emailErrorLabel').style.display = "inline";
-            document.querySelector('#emailErrorLabel').innerText = emailError;
-        };
+            document.querySelector('#email').onblur = function () {
+                var emailInput = $('#email').val();
+                var emailError = "";
+                if ("" == emailInput || null == emailInput) {
+                    emailError = "Please input your Email."
+                }
+                document.querySelector('#emailErrorLabel').style.display = "inline";
+                document.querySelector('#emailErrorLabel').innerText = emailError;
+            };
 
-        document.querySelector('#reemail').onblur = function () {
-            var reemailInput = $('#reemail').val();
-            var emailInput = $('#email').val();
-            var reemailError = "";
-            if (reemailInput != emailInput) {
-                reemailError = "Inputted Emails do not match."
-            }
-            document.querySelector('#reemailErrorLabel').style.display = "inline";
-            document.querySelector('#reemailErrorLabel').innerText = reemailError;
-        };
+            document.querySelector('#reemail').onblur = function () {
+                var reemailInput = $('#reemail').val();
+                var emailInput = $('#email').val();
+                var reemailError = "";
+                if (reemailInput != emailInput) {
+                    reemailError = "Inputted Emails do not match."
+                }
+                document.querySelector('#reemailErrorLabel').style.display = "inline";
+                document.querySelector('#reemailErrorLabel').innerText = reemailError;
+            };
 
-        document.querySelector('#mobilenumber').onblur = function () {
-            var mobilenumberInput = $('#mobilenumber').val();
-            var mobilenumberError = "";
-            if ("" == mobilenumberInput || null == mobilenumberInput) {
-                mobilenumberError = "Please input your Mobile Number."
-            }
-            document.querySelector('#mobilenumberErrorLabel').style.display = "inline";
-            document.querySelector('#mobilenumberErrorLabel').innerText = mobilenumberError;
-        };
-
+            document.querySelector('#mobilenumber').onblur = function () {
+                var mobilenumberInput = $('#mobilenumber').val();
+                var mobilenumberError = "";
+                if ("" == mobilenumberInput || null == mobilenumberInput) {
+                    mobilenumberError = "Please input your Mobile Number."
+                }
+                document.querySelector('#mobilenumberErrorLabel').style.display = "inline";
+                document.querySelector('#mobilenumberErrorLabel').innerText = mobilenumberError;
+            };
+        }
     });
 
     function onDeviceReady() {
@@ -111,11 +113,11 @@
     function insertToDB() {
         var ccnumber = $('#ccnumber').val();
         var fullname = $('#fullname').val();
-        var password = $('#password').val();
+        var password = $('#passwordField').val();
         var birthdate = $('#birthdate').val();
         var mobilenumber = $('#mobilenumber').val();
         var email = $('#email').val();
-        var url = "http://localhost:8080/registerUser?" + "pEMail=" + email + "&pFullname=" + fullname + "&pCCNumber=" + ccnumber + "&pMobileNumber=" + mobilenumber + "&pPassword=" + password + "&pBirthdate=" + birthdate;
+        var url = "http://192.168.100.16:8080/registerUser?" + "pEMail=" + email + "&pFullname=" + fullname + "&pCCNumber=" + ccnumber + "&pMobileNumber=" + mobilenumber + "&pPassword=" + password + "&pBirthdate=" + birthdate;
         $.post(url,
         function (response) {
             if (response == null || response == 'undefined') {
